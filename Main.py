74f53beb73180c482d093d695414d2b5130192c3
Alejandro Zapett <gunsn_1rosesslash@hotmail.com>
2019-02-25 21:29:10 -0600

    corrigiendo constructor, añadiendo comentarios y corrigiendo conseguir estado incial

--- Main.py
+++ Main.py
@@ -3,11 +3,16 @@ from Pila import Pila
 import sys 
 
 class Main(object):
-	def  __initi__(self, estado_inicial, estado_final):
+
+	def  __init__(self):
 		self.estado_inicial = self.conseguir_estado_inicial()
 		ruta=self.conseguir_ruta()
+
 	def conseguir_ruta(self, estado):
+		#condiciones iniciales del problema
 		estado_actual= estado
+
+		#Busqueda por profundidad (DFS)
 		while (es_solucion(estado_actual.conseguir_tablero())):
 		 	hijo= crear_hijo(estado_actual)
 		 	if hijo != 0:
@@ -18,10 +23,10 @@ class Main(object):
 		return estado_actual
 		 	
 	def crear_hijo(self, estado ):
+		print ("Implementar método...")
 		
 	def crear_hermano(self, estado ):
-
-
+		print ("Implementar método...")
 
 	def es_solucion(self, estado_actual):
 		contador = 0
@@ -33,11 +38,12 @@ class Main(object):
 			return False
 		else:
 			return True
+			
  	def conseguir_estado_inicial(self):
  		archivo_estado = open("estados.txt","r").read().split("\n")
  		tablero = []
  		for x in archivo_estado:
- 			tablero.append([number(y) for y in x.split(",")])
+ 			tablero.append([int(y) for y in x.split(",")])
  		return Nodo(tablero)
 
 
