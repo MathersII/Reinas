d3c3d43868f167d2bff9a4a3b92046c9014dab9f
Alejandro Zapett <gunsn_1rosesslash@hotmail.com>
2019-02-27 20:27:05 -0600

    arreglando problemas de mutabilidad en la búsqueda de posiciones

--- Main.py
+++ Main.py
@@ -1,44 +1,31 @@
 from Nodo import Nodo
 from Pila import Pila
-import sys 
+import sys
+import copy 
 
 class Main(object):
 
 	def  __init__(self):
-		
+		self.visitados = []
+		self.index = 0
 		self.estado_inicial = self.conseguir_estado_inicial()
 		ruta = self.conseguir_ruta(self.estado_inicial)
-		#for x in ruta:
-		#	self.imprime_matriz(x)
+		for x in ruta.conseguir_ruta_nodo():
+			self.imprime_matriz(x)
 		#self.imprime_matriz(ruta.conseguir_tablero())
 		
-		#self.pruebas()
+		self.pruebas()
+
 
 	def conseguir_ruta(self, estado):
-		""" Por recursividad
-		#condiciones iniciales del problema
-		estado_actual= estado
-
-		#Busqueda por profundidad (DFS)
-		while (es_solucion(estado_actual.conseguir_tablero())):
-		 	hijo= crear_hijo(estado_actual)
-		 	if hijo != 0:
-
-		 		estado_actual=self.conseguir_ruta(hijo)
-		 	else:
-		 		estado_actual= self.crear_hijo(estado_actual.conseguir_padre())
-		return estado_actual
-		"""
-
+		
 		#Condiciones iniciales
 		estado_actual = estado
-		#estado_actual.establecer_ruta_nodo(estado_actual.conseguir_ruta_nodo(), estado_actual.conseguir_tablero())
 		pila = Pila()
-		#ruta_al_nodo = [estado_actual.conseguir_tablero()]
 		visitados = []
+
 		#Busqueda por profundidad (DFS)
 		while(self.es_solucion(estado_actual)):
-			#self.imprime_matriz(estado_actual.conseguir_tablero())
 			visitados.append(estado_actual.conseguir_tablero())
 			hijos = self.crear_hijos(estado_actual)
 			if len(hijos) != 0:
@@ -46,39 +33,26 @@ class Main(object):
 					if x.conseguir_tablero() not in visitados:
 						x.establecer_ruta_nodo(estado_actual.conseguir_ruta_nodo(), estado_actual.conseguir_tablero())
 						pila.push(x)
-						self.imprime_matriz(visitados[len(visitados)-1])
-						#print("Hijo: \n")
-						#self.imprime_matriz(x.conseguir_tablero())
-						#print("======= \n")
-
 
 			estado_actual = pila.pop()
-			#self.imprime_matriz(estado_actual.conseguir_tablero())
-			visitados.append(estado_actual.conseguir_tablero())
-			self.imprime_matriz(visitados[len(visitados)-1])
 		estado_actual.establecer_ruta_nodo(estado_actual.conseguir_ruta_nodo(), estado_actual.conseguir_tablero())
-		return estado_actual.conseguir_ruta_nodo()
-
-		 	
-	def crear_hijo(self, estado ):
-		print ("Implementar método...")
-		
-	def crear_hermano(self, estado ):
-		print ("Implementar método...")
+		return estado_actual
 
 	def crear_hijos(self, estado):
+		posiciones = []
 		posiciones = self.busca_posiciones(estado)
 		if posiciones != -1:
 			Nodos = list(
 				map(
 					lambda x: Nodo(self.llena_posiciones_prohibidas(x,estado.conseguir_tablero())),posiciones
 					)
-				) 
+				)
 			return Nodos
 		else:
 			return []
 
 	def busca_posiciones(self, estado,lista = []):
+		lista = []
 		for idxx, valx in enumerate(estado.conseguir_tablero()):
 			for idxy, valy in enumerate(valx):
 				if valy == 0 :
@@ -89,7 +63,7 @@ class Main(object):
 		return lista
 					
 	def llena_posiciones_prohibidas(self,posiciones,estado):
-		tablero =  [x[:] for x in estado]
+		tablero =  copy.deepcopy(estado)
 		x = posiciones[0]
 		y = posiciones[1]
 		tablero[x][y] = 2
@@ -168,12 +142,7 @@ class Main(object):
 
 	def pruebas(self):
 		print  ("=== pruebas main ===")
-		estado_actual = self.conseguir_estado_inicial()
-		self.imprime_matriz(estado_actual.conseguir_tablero())
-		hijos = self.crear_hijos(estado_actual)
-		for x in hijos:
-			self.imprime_matriz(x.conseguir_tablero())
-		
+
 
 
 if __name__ == '__main__':
--- Nodo.py
+++ Nodo.py
@@ -2,6 +2,7 @@ class Nodo:
 	def __init__(self, lista):
 		self.lista=lista
 		self.ruta_al_nodo= []
+		self.padre = ""
 
 	def conseguir_tablero(self):
 		return self.lista
