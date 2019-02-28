6a05cc2f2639765980a2c7f0d19c5e00d272c992
berenice <bemesa21@gmail.com>
2019-02-27 21:05:29 -0600

    se anade solucion recusriva

--- Main.py
+++ Main.py
@@ -6,12 +6,28 @@ class Main(object):
 
 	def  __init__(self):
 		self.estado_inicial = self.conseguir_estado_inicial()
-		ruta = self.conseguir_ruta(self.estado_inicial)
+		#ruta = self.conseguir_ruta_iterativa(self.estado_inicial)
+		ruta = self.conseguir_ruta_recursivamente(self.estado_inicial)[1]
 		for x in ruta:
 			self.imprime_matriz(x)
-		#self.pruebas()
 
-	def conseguir_ruta(self, estado):
+
+	def conseguir_ruta_recursivamente(self, estado, solucion = []):
+		#self.imprime_matriz(estado.conseguir_tablero())
+		if(self.es_solucion(estado) == False):
+			solucion.insert(0,estado.conseguir_tablero())
+			return (True,solucion)
+		#Busqueda por profundidad (DFS)
+		hijos = self.crear_hijos(estado)
+		estado.set_hijos(hijos)
+		while(len(estado.get_hijos()) > 0):
+			if(self.conseguir_ruta_recursivamente(estado.get_hijo())[0]):
+				solucion.insert(0,estado.conseguir_tablero())
+				return (True,solucion)
+		return (False,solucion)
+
+
+	def conseguir_ruta_iterativa(self, estado):
 		#Condiciones iniciales
 		estado_actual = estado
 		pila = Pila()
--- Nodo.py
+++ Nodo.py
@@ -9,4 +9,10 @@ class Nodo:
 		return self.ruta_al_nodo
 	def establecer_ruta_nodo(self, ruta , tablero):
 		self.ruta_al_nodo= ruta+[tablero]
+	def set_hijos(self,hijos):
+		self.hijos = hijos
+	def get_hijos(self):
+		return self.hijos 
+	def get_hijo(self):
+		return self.hijos.pop()
 
